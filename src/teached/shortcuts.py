963eb77123f1c3a2f08f88479bb7f4df9eaab7f4
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-06-16 11:53:21 +0300

    update dependencies, add add upload_to_dropbox function

--- src/teached/shortcuts.py
+++ src/teached/shortcuts.py
@@ -10,3 +10,21 @@ def get_user_model() -> Any:
     module_name, class_name = settings.User_MODEL.rsplit(".", 1)
 
     return getattr(importlib.import_module(module_name), class_name)
+
+
+def upload_to_dropbox(
+    *, oauth2_token: str, file: bytes, filename: str, file_path: str
+) -> None:
+    """Helper function that upload files to dropbox.
+
+    Args:
+        oauth2_token: DropBox token.
+        file: The file in bytes.
+        filename: The file name.
+        file_path: A file path to store in dropbox.
+    """
+    import dropbox
+
+    dbx = dropbox.Dropbox(oauth2_token)
+    dbx.users_get_current_account()
+    dbx.files_upload(file, f"{file_path}/{filename}")
