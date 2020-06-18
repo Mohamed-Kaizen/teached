f02efefb1093224cdeace2741db7c882635880db
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-06-18 07:47:54 +0300

    update models.py

--- src/teached/users/models.py
+++ src/teached/users/models.py
@@ -1,11 +1,45 @@
 """Models for users app."""
+from tortoise import fields, models
+
 from .base import AbstractUser
 
 
 class User(AbstractUser):
     """The User model."""
 
+    full_name = fields.CharField(max_length=100, null=True)
+
+    phone_number = fields.CharField(max_length=15, null=True)
+
+    bio = fields.TextField(null=True)
+
     class Meta:
         """Meta data."""
 
         table = "user"
+
+
+class Teacher(models.Model):
+    """The teacher model."""
+
+    user = models.OneToOneFieldInstance(
+        "models.User", related_name="teachers", on_delete=fields.CASCADE
+    )
+
+    class Meta:
+        """Meta data."""
+
+        table = "teacher"
+
+
+class Student(models.Model):
+    """The student model."""
+
+    user = models.OneToOneFieldInstance(
+        "models.User", related_name="students", on_delete=fields.CASCADE
+    )
+
+    class Meta:
+        """Meta data."""
+
+        table = "student"
