bb5a74a416794e90f3b9e6eb037b9d9d2044b5ce
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-05 09:08:06 +0300

    add course filter

--- src/teached/courses/services.py
+++ src/teached/courses/services.py
@@ -1,5 +1,7 @@
 """Collection of services."""
-from typing import Dict
+from typing import Dict, Optional
+
+from tortoise import QuerySet
 
 from teached.users.models import Teacher
 
@@ -44,3 +46,48 @@ async def create_course(*, data: Dict, teacher: Teacher) -> str:
         await Requirement.create(name=requirement.capitalize(), course=course)
 
     return course.slug
+
+
+async def get_published_courses(
+    *,
+    search: Optional[str] = None,
+    category: Optional[str] = None,
+    language: Optional[str] = None,
+    level: Optional[str] = None,
+    price: Optional[str] = None,
+    discount: Optional[str] = None,
+) -> QuerySet[Course]:
+    """Return all published courses.
+
+    Args:
+        search: Search courses by title.
+        category: Filter by category.
+        language: Filter by language.
+        level: Filter by level.
+        price: Filter by price.
+        discount: Filter by discount.
+
+    Returns:
+        Query set of course.
+    """
+    courses = Course.filter(is_drift=True, is_active=True)
+
+    if search:
+        courses = courses.filter(title=search)
+
+    if category:
+        courses = courses.filter(categories__name=category)
+
+    if language:
+        courses = courses.filter(languages__name=language)
+
+    if level:
+        courses = courses.filter(level=level)
+
+    if price:
+        courses = courses.filter(price=price)
+
+    if discount:
+        courses = courses.filter(discount=discount)
+
+    return courses
--- src/teached/courses/views.py
+++ src/teached/courses/views.py
@@ -6,18 +6,31 @@ from fastapi import APIRouter, Depends, status
 from teached.users import depends, models
 
 from . import schema  # noqa I202
-from .models import Course, CourseListPydantic
-from .services import create_course
+from .models import CourseListPydantic
+from .services import create_course, get_published_courses
 
 router = APIRouter()
 
 
 @router.get("/")
-async def course_list() -> List[CourseListPydantic]:
+async def course_list(
+    search: str = None,
+    category: str = None,
+    language: str = None,
+    level: str = None,
+    price: str = None,
+    discount: str = None,
+) -> List[CourseListPydantic]:
     """Courses list."""
-    # TODO: Change is_drift to False
     return await CourseListPydantic.from_queryset(
-        Course.filter(is_drift=True, is_active=True)
+        await get_published_courses(
+            search=search,
+            category=category,
+            language=language,
+            level=level,
+            price=price,
+            discount=discount,
+        )
     )
 
 
@@ -31,3 +44,15 @@ async def course_create(
     )
 
     return {"slug": slug}
+
+
+#
+# @router.get("/{slug}/")
+# async def course_detail(slug: str) -> Dict[str, str]:
+#     """Course detail."""
+#
+#     slug = await create_course(
+#         data=user_input.dict(exclude_unset=True), teacher=auth_user
+#     )
+#
+#     return {"slug": slug}
